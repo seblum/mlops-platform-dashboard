8999a641256167177541c5d1dad7cc346ef4bb77
Sebastian Blum <sebast.blum@gmail.com>
2023-08-19 10:39:52 +0200

    added full size iframe

--- vue-dashboard/src/pages/Grafana.vue
+++ vue-dashboard/src/pages/Grafana.vue
@@ -1,18 +1,10 @@
 
 <template>
   <div class="content">
-    <div class="md-layout">
-      <div class="md-layout-item">
-        <md-card class="md-card-plain">
-          <md-card-content>
-            <div class="iframe-container hidden-sm">
-              <iframe src="http://mlplatform.seblum.me/grafana">
-                <p>Your browser does not support iframes.</p>
-              </iframe>
-            </div>
-          </md-card-content>
-        </md-card>
-      </div>
+    <div class="iframe-container">
+      <iframe src="http://www.seblum.me/mlops" >
+        <p>Your browser does not support iframes.</p>
+      </iframe>
     </div>
   </div>
 </template>
